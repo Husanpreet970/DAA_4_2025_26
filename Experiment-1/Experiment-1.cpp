bb09295d4b164ea7fc757698ec3140338ff7ec49
Husanpreet Kaur <husankajji@gmail.com>
2026-01-29 21:46:59 +0530

    Experiment-1.cpp

--- Experiment-1/Experiment-1.cpp
+++ Experiment-1/Experiment-1.cpp
@@ -1,1 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace std::chrono;
+int operations=0;
+int maxvalue = 0;
+int current_value=0;
+int maximumdepth;
+void complexRec(int n) {
+    current_value++;
+    maximumdepth = max(maxvalue, current_value);
+   if (n <= 2) {
+       return;
+   }
+   int p = n;
+   while (p > 0) {
+       vector<int> temp(n);
+       for (int i = 0; i < n; i++) {
+           temp[i] = i ^ p;
+           operations++;
+       }
+       p >>= 1;
+   }
+   vector<int> small(n);
+   for (int i = 0; i < n; i++) {
+       small[i] = i * i;
+       operations++;
+   }
+   if (n % 3 == 0) {
+       reverse(small.begin(), small.end());
+   } else {
+       reverse(small.begin(), small.end());
+   }
+   complexRec(n / 2);
+   complexRec(n / 2);
+   complexRec(n / 2);
+}
+int main()
+{
+      int n=2000;
+      auto start=high_resolution_clock::now();
+      complexRec(n);
+      auto end=high_resolution_clock::now();
+      auto duration=duration_cast<milliseconds>(end-start);
+      cout<<"Operations:"<<operations<<endl;
+      cout<<"Duration: "<<duration.count()<<endl;
+      cout<<"Max.depth: "<<maximumdepth<<endl;
+}
 
+//1. Recurrence relation: 3T(n/2)+n log n
+//For while loop:- O(n log n)
+//Reverse:- O(n)
+//2. Asymptotic complexity: T(n)=aT(n/b)+f(n)
+//where a=3,b=2
+//T(n)= theta(n^1.585) [Case 1: f(n)<n^k]
